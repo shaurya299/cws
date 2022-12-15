30f3bb6d62f2e97744fc327ee32e68f37da4d49d
shaurya299 <117354944+shaurya299@users.noreply.github.com>
2022-12-15 16:55:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,8 +61,8 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.src = `${songIndex+1}.mp3`;
+        masterSongName.innerText = [songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
         gif.style.opacity = 1;
@@ -78,8 +78,8 @@ document.getElementById('next').addEventListener('click', ()=>{
     else{
         songIndex += 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.src = `${songIndex+1}.mp3`;
+    masterSongName.innerText = [songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
@@ -94,8 +94,8 @@ document.getElementById('previous').addEventListener('click', ()=>{
     else{
         songIndex -= 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.src = `${songIndex+1}.mp3`;
+    masterSongName.innerText = [songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
